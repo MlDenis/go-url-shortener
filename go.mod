a1b74946f2b5ec25a48d5c6050aea895d2ee98bd
MLDenis <malyavin.dd@icloud.com>
2023-07-07 13:16:11 +0300

    Added implementation of a server that handles get, post, and unknown type requests.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MlDenis/go-url-shortener
 
 go 1.21rc2
+
+require github.com/google/uuid v1.3.0
--- internal/app/server.go
+++ internal/app/server.go
@@ -1,25 +1,68 @@
 package app
 
-import "fmt"
+import (
+	"github.com/MlDenis/go-url-shortener/internal/config"
+	"io"
+	"net/http"
+	"path"
+	"github.com/google/uuid"
+)
 
-type Config struct {
-	BaseURL      string
-	ServerAdress string
+func Run() error {
+	cfg, err := config.NewConfig()
+	if err != nil {
+		return err
+	}
+
+	http.HandleFunc("/", ServeTheRequest)
+	http.ListenAndServe(cfg.BaseURL, nil)
+
+	return nil
 }
 
-func NewConfig() (*Config, error) {
-	cfg := &Config{}
-	cfg.BaseURL = ":8080"
-	cfg.ServerAdress = "http://localhost:8080"
-	return cfg, nil
+func ServeTheRequest(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		GetHandler(w, r)
+	case http.MethodPost:
+		PostHandler(w, r)
+	default:
+		BadRequestHandler(w, r)
+	}
 }
 
-// String returns values of Config structure.
-func (cfg *Config) String() string {
-	return fmt.Sprintf(
-		"BASE_URL: %s\n"+
-			"SERVER_ADDRESS: %s\n",
-		cfg.BaseURL,
-		cfg.ServerAdress,
-	)
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	_, shortURL := path.Split(r.URL.Path)
+	URL, found := URLsArchive[shortURL]
+	if != found {
+		BadRequestHandler(w, r)
+	}
+
+	http.Redirect(w, r, URL, http.StatusTemporaryRedirect)
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+	shortURL := generateShortURL(string(body))
+	w.Header().Set("content-type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(shortURL))
+
+}
+
+func generateShortURL(URL string) string {
+	if value, found := URLsArchive[URL]; found {
+		return value
+	}
+
+	shortURL := uuid.New().String()
+	URLsArchive[shortURL] = URL
+	return shortURL
+}
+
+func BadRequestHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "", http.StatusBadRequest)
 }

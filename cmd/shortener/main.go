9596f7b0979ccac1d31a564bf5e8b478fc854889
MLDenis <malyavin.dd@icloud.com>
2023-07-07 14:39:09 +0300

    Added the implementation of starting the server at the entry point to the program.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"github.com/MlDenis/go-url-shortener/internal/app"
+	"log"
+)
+
+func main() {
+	err := app.Run()
+	if err != nil {
+		log.Fatal("Server startup error", err)
+	}
+}
